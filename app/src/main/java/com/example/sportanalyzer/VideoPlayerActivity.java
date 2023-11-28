7f34e018bceacb7218fc5d5bfa3f4b54a822d2a1
LENOVO <naifnazz786@gmail.com>
2023-11-28 14:00:59 +0530

    sportanalyzer

--- app/src/main/java/com/example/sportanalyzer/VideoPlayerActivity.java
+++ app/src/main/java/com/example/sportanalyzer/VideoPlayerActivity.java
@@ -11,16 +11,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 
 
 public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHolder.Callback {
@@ -32,13 +31,16 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
     private SeekBar seekBar;
     private Handler handler;
     private boolean pause = false;
+    private Button pauseButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videopleyer);
-
+        SurfaceView surfaceView = findViewById(R.id.videoview);
+        surfaceView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        surfaceView.setLayerType(SurfaceView.LAYER_TYPE_HARDWARE, null);
         surfaceView = findViewById(R.id.videoview);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
@@ -51,12 +53,15 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        seekBar.setMax(100);
+        seekBar.setProgress(0);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser) {
-                    seekTo(progress);
-                }
+
+                seekTo(progress);
+
             }
 
             @Override
@@ -73,108 +78,62 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
         handler = new Handler(Looper.getMainLooper());
     }
 
-        @Override
-        public void surfaceCreated (SurfaceHolder holder){
-            mediaPlayer.setDisplay(holder);
-            mediaPlayer.start();
+    @Override
+    public void surfaceCreated (SurfaceHolder holder){
+        mediaPlayer.setDisplay(holder);
 
-        }
+        mediaPlayer.start();
 
-        @Override
-        public void surfaceChanged (SurfaceHolder holder,int format, int width, int height){
-            // Handle surface changes if needed
-        }
 
-        @Override
-        public void surfaceDestroyed (SurfaceHolder holder){
-            // Release MediaPlayer when the surface is destroyed
-            if (mediaPlayer != null) {
-                mediaPlayer.release();
-            }
-        }
-
-        @Override
-        protected void onDestroy () {
-            super.onDestroy();
-            // Release MediaPlayer when the activity is destroyed
-            if (mediaPlayer != null) {
-                mediaPlayer.release();
-            }
-        }
-
-        private void initializeMediaCodec () {
-            try {
-                mediaExtractor = new MediaExtractor();
-                mediaExtractor.setDataSource("Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI");
-
-                MediaFormat format = mediaExtractor.getTrackFormat(0);
-                String mime = format.getString(MediaFormat.KEY_MIME);
-                mediaCodec = MediaCodec.createDecoderByType(mime);
-                Surface surface = null;
-                mediaCodec.configure(format, surface, null, 0);
-                mediaCodec.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void extractAndDisplayFrames () {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    ByteBuffer[] inputBuffers = mediaCodec.getInputBuffers();
-
-                    while (true) {
-                        if (!pause) {
-                            int inputBufferIndex = mediaCodec.dequeueInputBuffer(10000);
-                            if (inputBufferIndex >= 0) {
-                                ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
-                                int sampleSize = mediaExtractor.readSampleData(inputBuffer, 0);
-
-                                if (sampleSize < 0) {
-                                    mediaCodec.queueInputBuffer(inputBufferIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
-                                    break;
-                                } else {
-                                    mediaCodec.queueInputBuffer(inputBufferIndex, 0, sampleSize, mediaExtractor.getSampleTime(), 0);
-                                    mediaExtractor.advance();
-                                }
-                            }
-                            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
-                            int outputBufferIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, 10000);
-
-                            if (outputBufferIndex >= 0) {
-                                mediaCodec.releaseOutputBuffer(outputBufferIndex, true);
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        int currentPosition = (int) mediaExtractor.getSampleTime() / 1000;
-                                        seekBar.setProgress(currentPosition);
-                                    }
-                                });
-                            }
-
-                            if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }).start();
-        }
-    private void setupTimeline() {
-        int duration = (int) mediaExtractor.getSampleTime() / 1000; // Convert to milliseconds
-        seekBar.setMax(duration);
-        handler.postDelayed(updateSeekBar, 1000);
     }
 
-    private void seekTo(int position) {
-        long seekPosition = position * 1000L; // Convert to microseconds
-        if (mediaExtractor != null) {
-            // Use logging to debug and trace the flow
-            Log.d("VideoActivity", "Before seekTo");
-            mediaExtractor.seekTo(seekPosition, MediaExtractor.SEEK_TO_CLOSEST_SYNC);
-            Log.d("VideoActivity", "After seekTo");
+    @Override
+    public void surfaceChanged (SurfaceHolder holder,int format, int width, int height){
+        // Handle surface changes if needed
+    }
+
+    @Override
+    public void surfaceDestroyed (SurfaceHolder holder){
+        // Release MediaPlayer when the surface is destroyed
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
         }
+    }
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+        // Release MediaPlayer when the activity is destroyed
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
+
+    private void initializeMediaCodec () {
+        try {
+            mediaExtractor = new MediaExtractor();
+            mediaExtractor.setDataSource("Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI");
+
+            MediaFormat format = mediaExtractor.getTrackFormat(0);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            mediaCodec = MediaCodec.createDecoderByType(mime);
+            Surface surface = null;
+            mediaCodec.configure(format, surface, null, 0);
+            mediaCodec.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private void seekTo(int position) {
+
+        long fullduration=mediaPlayer.getDuration();
+        int cal=(int)(fullduration* (position/100.0));
+
+        runOnUiThread(() -> mediaPlayer.seekTo(cal));
+
     }
 
     private Runnable updateSeekBar = new Runnable() {
@@ -200,11 +159,18 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
 
     // Toggle between pause and resume
     private void togglePauseResume() {
-        pause = !pause;
-        if (pause) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
             Toast.makeText(this, "Paused", Toast.LENGTH_SHORT).show();
+
+            // Stop updating the seek bar
+            handler.removeCallbacks(updateSeekBar);
         } else {
+            mediaPlayer.start();
             Toast.makeText(this, "Resumed", Toast.LENGTH_SHORT).show();
+
+            // Start updating the seek bar again
+            handler.postDelayed(updateSeekBar, 1000);
         }
     }
 
@@ -213,9 +179,3 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
         togglePauseResume();
     }
 }
-
-
-
-
-
-
